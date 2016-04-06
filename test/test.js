fab33d0c24fa32233a39e3d77a1bab2e69759c24
Daniel Morrison <daniel@collectiveidea.com>
2016-04-05 22:55:21 -0400

    Refactor JS to unify field names and Result keys

--- test/test.js
+++ test/test.js
@@ -32,7 +32,7 @@ casper.test.begin("Math works with default values", 12, function suite(test) {
 
   casper.then(function() {
     test.assertSelectorHasText('td [data-name="projected_internet"]', '$32');
-    test.assertSelectorHasText('td [data-name="monthly_total"]', '$115');
+    test.assertSelectorHasText('td [data-name="current_monthly"]', '$115');
     test.assertSelectorHasText('td [data-name="projected_total"]', '$54.81');
     test.assertSelectorHasText('td [data-name="fiber_millage"]', '$13.81');
     test.assertSelectorHasText('td [data-name="annual_savings"]', '$722.28');
@@ -53,7 +53,7 @@ casper.test.begin("Math works when increasing taxable value", 4, function suite(
   });
 
   casper.then(function() {
-    test.assertSelectorHasText('td [data-name="monthly_total"]', '$115');
+    test.assertSelectorHasText('td [data-name="current_monthly"]', '$115');
     test.assertSelectorHasText('td [data-name="projected_total"]', '$75.53');
     test.assertSelectorHasText('td [data-name="fiber_millage"]', '$34.53');
     test.assertSelectorHasText('td [data-name="annual_savings"]', '$473.64');
@@ -73,7 +73,7 @@ casper.test.begin("Math works when decreasing taxable value", 4, function suite(
   });
 
   casper.then(function() {
-    test.assertSelectorHasText('td [data-name="monthly_total"]', '$115');
+    test.assertSelectorHasText('td [data-name="current_monthly"]', '$115');
     test.assertSelectorHasText('td [data-name="projected_total"]', '$47.91');
     test.assertSelectorHasText('td [data-name="fiber_millage"]', '$6.91');
     test.assertSelectorHasText('td [data-name="annual_savings"]', '$805.08');
@@ -95,7 +95,7 @@ casper.test.begin("Properly rounds to two decimals", 4, function suite(test) {
   });
 
   casper.then(function() {
-    test.assertSelectorHasText('td [data-name="monthly_total"]', '$115');
+    test.assertSelectorHasText('td [data-name="current_monthly"]', '$115');
     test.assertSelectorHasText('td [data-name="projected_total"]', '$78.60');
     test.assertSelectorHasText('td [data-name="fiber_millage"]', '$7.60');
     test.assertSelectorHasText('td [data-name="annual_savings"]', '$436.80');
@@ -120,7 +120,7 @@ casper.test.begin("Works with non-numbers", 4, function suite(test) {
   });
 
   casper.then(function() {
-    test.assertSelectorHasText('td [data-name="monthly_total"]', '$0');
+    test.assertSelectorHasText('td [data-name="current_monthly"]', '$0');
     test.assertSelectorHasText('td [data-name="projected_total"]', '$32');
     test.assertSelectorHasText('td [data-name="fiber_millage"]', '$0');
     test.assertSelectorHasText('td [data-name="annual_savings"]', '$-384');
